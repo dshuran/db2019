8fad61523bd1cfb5e229171509852137d3292fdb
dshuran <dmitry.shuran@gmail.com>
2019-07-27 23:46:29 +0300

    waypoint sequence works

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import getCoordinatesFromAdresses from './adressesToMarkers.js'
 import coordsToRoute from './route.js'
 import {deployBubblesGroup} from './mapBubbles.js';
+import getBestSequence from '../waypointSequence.js';
 
-getCoordinatesFromAdresses(['Moscow', 'Minsk'])
+getCoordinatesFromAdresses(['Moscow', 'Paris', 'Berlin', 'Minsk'])
     .then((result) => {
         deployBubblesGroup();
-        coordsToRoute(result);
-    });
+        console.log(result.length);
+        return getBestSequence(result);
+    }).then((res) => {
+        let rightCords = [];
+        for (let obj of res.results[0].waypoints)
+        {
+            let lat = obj.lat;
+            let lng = obj.lng;
+            rightCords.push({lat, lng});
+        }
+        coordsToRoute(rightCords);
+    })
 // coordsToRoute([{lat: '0.5309916298853', lng: '0.5309916298853'}, {lat: '0.5409916298853', lng: '0.5309916298853'}])
