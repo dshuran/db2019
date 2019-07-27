e7453910be68059adfee16e5f86c152e75b51f2b
dshuran <dmitry.shuran@gmail.com>
2019-07-27 22:07:29 +0300

    added bubles on tap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import getCoordinatesFromAdresses from './adressesToMarkers.js'
 import coordsToRoute from './route.js'
+import addInfoBubble from './mapBubbles.js';
 
 getCoordinatesFromAdresses(['Moscow', 'Minsk'])
     .then(coordsToRoute)
+    .then(() => 
+    {
+        addInfoBubble(map);
+    })
 // coordsToRoute([{lat: '0.5309916298853', lng: '0.5309916298853'}, {lat: '0.5409916298853', lng: '0.5309916298853'}])
---
+++ src/mapBubbles.js
@@ -0,0 +1,39 @@
+/**
+ * Creates a new marker and adds it to a group
+ * @param {H.map.Group} group       The group holding the new marker
+ * @param {H.geo.Point} coordinate  The location of the marker
+ * @param {String} html             Data associated with the marker
+ */
+function addMarkerToGroup(group, coordinate, html) {
+  var marker = new H.map.Marker(coordinate);
+  // add custom data to the marker
+  marker.setData(html);
+  group.addObject(marker);
+}
+
+/**
+ * Add two markers showing the position of Liverpool and Manchester City football clubs.
+ * Clicking on a marker opens an infobubble which holds HTML content related to the marker.
+ * @param  {H.Map} map      A HERE Map instance within the application
+ */
+function addInfoBubble(map) {
+  var group = new H.map.Group();
+
+  map.addObject(group);
+
+  // add 'tap' event listener, that opens info bubble, to the group
+  group.addEventListener('tap', function (evt) {
+    // event target is the marker itself, group is a parent event target
+    // for all objects that it contains
+    var bubble =  new H.ui.InfoBubble(evt.target.getGeometry(), {
+      // read custom data
+      content: evt.target.getData()
+    });
+    // show info bubble
+    ui.addBubble(bubble);
+  }, false);
+
+  addMarkerToGroup(group, {lat:53.439, lng:-2.221}, 'Hello');
+}
+
+export default addInfoBubble;
